a0db4343f68393bc8c02e7560bbfbaccce25c411
alidadayev <dadayev95@gmail.com>
2022-08-30 11:56:37 +0200

    update 02

--- web/src/App.js
+++ web/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn DevOps
+          Learn DevOps Today
         </a>
         <button>Login</button>
       </header>
